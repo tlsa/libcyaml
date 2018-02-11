bc6c3cf3977324f09dc3b366b3706317cadeacfa
Michael Drake <tlsa@netsurf-browser.org>
2018-02-11 12:09:40 +0000

    Tests: Add test for allocated sequence of boolean values.

--- test/units/load.c
+++ test/units/load.c
@@ -1223,6 +1223,68 @@ static bool test_load_mapping_entry_sequence_ptr_uint(
 	return ttest_pass(&tc);
 }
 
+static bool test_load_mapping_entry_sequence_ptr_bool(
+		ttest_report_ctx_t *report,
+		const cyaml_config_t *config)
+{
+	bool ref[] = { true, false, true, false, true, false, true, false };
+	static const unsigned char yaml[] =
+		"sequence:\n"
+		"    - true\n"
+		"    - false\n"
+		"    - yes\n"
+		"    - no\n"
+		"    - enable\n"
+		"    - disable\n"
+		"    - 1\n"
+		"    - 0\n";
+	struct target_struct {
+		bool *seq;
+		uint32_t seq_count;
+	} *data_tgt = NULL;
+	static const struct cyaml_schema_type entry_schema = {
+		CYAML_TYPE_BOOL(CYAML_FLAG_DEFAULT, *(data_tgt->seq)),
+	};
+	static const struct cyaml_schema_mapping mapping_schema[] = {
+		CYAML_MAPPING_SEQUENCE("sequence", CYAML_FLAG_POINTER,
+				struct target_struct, seq, &entry_schema,
+				0, CYAML_UNLIMITED),
+		CYAML_MAPPING_END
+	};
+	static const struct cyaml_schema_type top_schema = {
+		CYAML_TYPE_MAPPING(CYAML_FLAG_DEFAULT,
+				struct target_struct, mapping_schema),
+	};
+	test_data_t td = {
+		.data = (cyaml_data_t **) &data_tgt,
+		.config = config,
+		.schema = &top_schema,
+	};
+	cyaml_err_t err;
+
+	ttest_ctx_t tc = ttest_start(report, __func__, cyaml_cleanup, &td);
+
+	err = cyaml_load_data(yaml, YAML_LEN(yaml), config, &top_schema,
+			(cyaml_data_t **) &data_tgt);
+	if (err != CYAML_OK) {
+		return ttest_fail(&tc, cyaml_strerror(err));
+	}
+
+	if (CYAML_ARRAY_LEN(ref) != data_tgt->seq_count) {
+		return ttest_fail(&tc, "Incorrect sequence count");
+	}
+
+	for (unsigned i = 0; i < CYAML_ARRAY_LEN(ref); i++) {
+		if (data_tgt->seq[i] != ref[i]) {
+			return ttest_fail(&tc, "Incorrect value (i=%u): "
+					"got: %i, expected: %i", i,
+					data_tgt->seq[i], ref[i]);
+		}
+	}
+
+	return ttest_pass(&tc);
+}
+
 bool load_tests(
 		ttest_report_ctx_t *rc,
 		cyaml_log_t log_level,
@@ -1269,6 +1331,7 @@ bool load_tests(
 	pass &= test_load_mapping_entry_sequence_ptr_int(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_enum(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_uint(rc, &config);
+	pass &= test_load_mapping_entry_sequence_ptr_bool(rc, &config);
 
 	return pass;
 }
