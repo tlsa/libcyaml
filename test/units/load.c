0d25fef1f905c0b94ad740dec1798b558d89adce
Michael Drake <tlsa@netsurf-browser.org>
2018-03-13 20:22:59 +0000

    Tests: Add test for allocated sequence of allocated strings.

--- test/units/load.c
+++ test/units/load.c
@@ -2000,6 +2000,72 @@ static bool test_load_mapping_entry_sequence_ptr_string(
 	return ttest_pass(&tc);
 }
 
+/* Test loading a sequence of strings to allocated array of
+ * allocated strings. */
+static bool test_load_mapping_entry_sequence_ptr_string_ptr(
+		ttest_report_ctx_t *report,
+		const cyaml_config_t *config)
+{
+	const char *ref[] = {
+		"This",
+		"is",
+		"merely",
+		"a",
+		"test",
+	};
+	static const unsigned char yaml[] =
+		"sequence:\n"
+		"    - This\n"
+		"    - is\n"
+		"    - merely\n"
+		"    - a\n"
+		"    - test\n";
+	struct target_struct {
+		char **seq;
+		uint32_t seq_count;
+	} *data_tgt = NULL;
+	static const struct cyaml_schema_type entry_schema = {
+		CYAML_TYPE_STRING(CYAML_FLAG_POINTER, *(data_tgt->seq),
+				0, CYAML_UNLIMITED),
+	};
+	static const struct cyaml_schema_mapping mapping_schema[] = {
+		CYAML_MAPPING_SEQUENCE("sequence", CYAML_FLAG_POINTER,
+				struct target_struct, seq, &entry_schema,
+				0, CYAML_ARRAY_LEN(ref)),
+		CYAML_MAPPING_END
+	};
+	static const struct cyaml_schema_type top_schema = {
+		CYAML_TYPE_MAPPING(CYAML_FLAG_POINTER,
+				struct target_struct, mapping_schema),
+	};
+	test_data_t td = {
+		.data = (cyaml_data_t **) &data_tgt,
+		.config = config,
+		.schema = &top_schema,
+	};
+	cyaml_err_t err;
+
+	ttest_ctx_t tc = ttest_start(report, __func__, cyaml_cleanup, &td);
+
+	err = cyaml_load_data(yaml, YAML_LEN(yaml), config, &top_schema,
+			(cyaml_data_t **) &data_tgt);
+	if (err != CYAML_OK) {
+		return ttest_fail(&tc, cyaml_strerror(err));
+	}
+
+	if (CYAML_ARRAY_LEN(ref) != data_tgt->seq_count) {
+		return ttest_fail(&tc, "Incorrect sequence count");
+	}
+
+	for (unsigned i = 0; i < CYAML_ARRAY_LEN(ref); i++) {
+		if (strcmp(data_tgt->seq[i], ref[i]) != 0) {
+			return ttest_fail(&tc, "Incorrect value (i=%u)", i);
+		}
+	}
+
+	return ttest_pass(&tc);
+}
+
 /* Test loading a sequence of mappings to allocated array mapping structs. */
 static bool test_load_mapping_entry_sequence_ptr_mapping(
 		ttest_report_ctx_t *report,
@@ -2460,6 +2526,7 @@ bool load_tests(
 	pass &= test_load_mapping_entry_sequence_ptr_flags(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_string(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_mapping(rc, &config);
+	pass &= test_load_mapping_entry_sequence_ptr_string_ptr(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_mapping_ptr(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_sequence_fixed_int(rc, &config);
 	pass &= test_load_mapping_entry_sequence_ptr_sequence_fixed_ptr_int(rc, &config);
