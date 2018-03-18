24036d40b5f86e6a0f2871e6534dfd65cfb5d4a7
Michael Drake <tlsa@netsurf-browser.org>
2018-03-18 15:26:10 +0000

    Tests: Add test for expecting mapping and reading scalar.

--- test/units/errs.c
+++ test/units/errs.c
@@ -448,6 +448,54 @@ static bool test_err_schema_expect_flags_read_scalar(
 	return ttest_pass(&tc);
 }
 
+/* Test loading when schema expects mapping, but YAML has scalar. */
+static bool test_err_schema_expect_mapping_read_scalar(
+		ttest_report_ctx_t *report,
+		const cyaml_config_t *config)
+{
+	static const unsigned char yaml[] =
+		"key: scalar\n";
+	struct value_s {
+		int a;
+	};
+	struct target_struct {
+		struct value_s test_value_mapping;
+	} *data_tgt = NULL;
+	static const struct cyaml_schema_mapping test_mapping_schema[] = {
+		CYAML_MAPPING_INT("a", CYAML_FLAG_DEFAULT, struct value_s, a),
+		CYAML_MAPPING_END
+	};
+	static const struct cyaml_schema_mapping mapping_schema[] = {
+		CYAML_MAPPING_MAPPING("key", CYAML_FLAG_DEFAULT,
+				struct target_struct, test_value_mapping,
+				test_mapping_schema),
+	};
+	static const struct cyaml_schema_type top_schema = {
+		CYAML_TYPE_MAPPING(CYAML_FLAG_POINTER,
+				struct target_struct, mapping_schema),
+	};
+	test_data_t td = {
+		.data = (cyaml_data_t **) &data_tgt,
+		.config = config,
+		.schema = &top_schema,
+	};
+	cyaml_err_t err;
+
+	ttest_ctx_t tc = ttest_start(report, __func__, cyaml_cleanup, &td);
+
+	err = cyaml_load_data(yaml, YAML_LEN(yaml), config, &top_schema,
+			(cyaml_data_t **) &data_tgt);
+	if (err != CYAML_ERR_INVALID_VALUE) {
+		return ttest_fail(&tc, cyaml_strerror(err));
+	}
+
+	if (data_tgt != NULL) {
+		return ttest_fail(&tc, "Data non-NULL on error.");
+	}
+
+	return ttest_pass(&tc);
+}
+
 /**
  * Run the CYAML error unit tests.
  *
@@ -494,6 +542,7 @@ bool errs_tests(
 	pass &= test_err_schema_expect_int_read_end_1(rc, &config);
 	pass &= test_err_schema_expect_int_read_end_2(rc, &config);
 	pass &= test_err_schema_expect_flags_read_scalar(rc, &config);
+	pass &= test_err_schema_expect_mapping_read_scalar(rc, &config);
 
 	return pass;
 }
