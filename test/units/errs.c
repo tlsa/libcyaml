3c69d49ff28ef19257fb41c362251ea70075bd24
Michael Drake <tlsa@netsurf-browser.org>
2018-03-22 20:43:32 +0000

    Tests: Add loading test for schema with string min > max.

--- test/units/errs.c
+++ test/units/errs.c
@@ -177,6 +177,48 @@ static bool test_err_schema_bad_type(
 	return ttest_pass(&tc);
 }
 
+/* Test loading with schema with string min greater than max. */
+static bool test_err_schema_string_min_max(
+		ttest_report_ctx_t *report,
+		const cyaml_config_t *config)
+{
+	static const unsigned char yaml[] =
+		"value: foo\n";
+	struct target_struct {
+		const char *value;
+	} *data_tgt = NULL;
+	static const struct cyaml_schema_mapping mapping_schema[] = {
+		CYAML_MAPPING_STRING_PTR("value", CYAML_FLAG_POINTER,
+				struct target_struct, value,
+				10, 9),
+		CYAML_MAPPING_END
+	};
+	static const struct cyaml_schema_type top_schema = {
+		CYAML_TYPE_MAPPING(CYAML_FLAG_POINTER,
+				struct target_struct, mapping_schema),
+	};
+	test_data_t td = {
+		.data = (cyaml_data_t **) &data_tgt,
+		.config = config,
+		.schema = &top_schema,
+	};
+	cyaml_err_t err;
+
+	ttest_ctx_t tc = ttest_start(report, __func__, cyaml_cleanup, &td);
+
+	err = cyaml_load_data(yaml, YAML_LEN(yaml), config, &top_schema,
+			(cyaml_data_t **) &data_tgt);
+	if (err != CYAML_ERR_BAD_MIN_MAX_SCHEMA) {
+		return ttest_fail(&tc, cyaml_strerror(err));
+	}
+
+	if (data_tgt != NULL) {
+		return ttest_fail(&tc, "Data non-NULL on error.");
+	}
+
+	return ttest_pass(&tc);
+}
+
 /* Test loading with schema with data size (0). */
 static bool test_err_schema_bad_data_size_1(
 		ttest_report_ctx_t *report,
@@ -580,6 +622,7 @@ bool errs_tests(
 	ttest_heading(rc, "Bad schema tests");
 
 	pass &= test_err_schema_bad_type(rc, &config);
+	pass &= test_err_schema_string_min_max(rc, &config);
 	pass &= test_err_schema_bad_data_size_1(rc, &config);
 	pass &= test_err_schema_bad_data_size_2(rc, &config);
 
