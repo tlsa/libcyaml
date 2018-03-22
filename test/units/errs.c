c711c8bf2908b64442658f8c59dce0d247b30ea3
Michael Drake <tlsa@netsurf-browser.org>
2018-03-22 20:47:38 +0000

    Tests: Add loading test for schema with invalid float data size.

--- test/units/errs.c
+++ test/units/errs.c
@@ -376,6 +376,54 @@ static bool test_err_schema_sequence_min_max(
 	return ttest_pass(&tc);
 }
 
+/* Test loading with schema with data size for float. */
+static bool test_err_schema_bad_data_size_float(
+		ttest_report_ctx_t *report,
+		const cyaml_config_t *config)
+{
+	static const unsigned char yaml[] =
+		"key: 1\n";
+	struct target_struct {
+		int value;
+	} *data_tgt = NULL;
+	static const struct cyaml_schema_mapping mapping_schema[] = {
+		{
+			.key = "key",
+			.value = {
+				.type = CYAML_FLOAT,
+				.flags = CYAML_FLAG_DEFAULT,
+				.data_size = 7,
+			},
+			.data_offset = offsetof(struct target_struct, value),
+		},
+		CYAML_MAPPING_END
+	};
+	static const struct cyaml_schema_type top_schema = {
+		CYAML_TYPE_MAPPING(CYAML_FLAG_POINTER,
+				struct target_struct, mapping_schema),
+	};
+	test_data_t td = {
+		.data = (cyaml_data_t **) &data_tgt,
+		.config = config,
+		.schema = &top_schema,
+	};
+	cyaml_err_t err;
+
+	ttest_ctx_t tc = ttest_start(report, __func__, cyaml_cleanup, &td);
+
+	err = cyaml_load_data(yaml, YAML_LEN(yaml), config, &top_schema,
+			(cyaml_data_t **) &data_tgt);
+	if (err != CYAML_ERR_INVALID_DATA_SIZE) {
+		return ttest_fail(&tc, cyaml_strerror(err));
+	}
+
+	if (data_tgt != NULL) {
+		return ttest_fail(&tc, "Data non-NULL on error.");
+	}
+
+	return ttest_pass(&tc);
+}
+
 /* Test loading when schema expects int, but YAML has sequence. */
 static bool test_err_schema_expect_int_read_seq(
 		ttest_report_ctx_t *report,
@@ -687,6 +735,7 @@ bool errs_tests(
 	pass &= test_err_schema_bad_data_size_1(rc, &config);
 	pass &= test_err_schema_bad_data_size_2(rc, &config);
 	pass &= test_err_schema_sequence_min_max(rc, &config);
+	pass &= test_err_schema_bad_data_size_float(rc, &config);
 
 	ttest_heading(rc, "YAML / schema mismatch: expected value type tests");
 
