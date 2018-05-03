7d966dcee05b3f39acd16267ce9c287b9b7341e2
Michael Drake <tlsa@netsurf-browser.org>
2018-05-03 18:08:08 +0100

    Load: Split state machine states out into util.
    They will be reused for save support.

--- src/util.h
+++ src/util.h
@@ -23,6 +23,38 @@ typedef uint32_t cyaml_bitfield_t;
 /** Number of bits in \ref cyaml_bitfield_t. */
 #define CYAML_BITFIELD_BITS (sizeof(cyaml_bitfield_t) * CHAR_BIT)
 
+/** CYAML state machine states. */
+enum cyaml_state_e {
+	CYAML_STATE_START,       /**< Initial state. */
+	CYAML_STATE_IN_STREAM,   /**< In a stream. */
+	CYAML_STATE_IN_DOC,      /**< In a document. */
+	CYAML_STATE_IN_MAPPING,  /**< In a mapping. */
+	CYAML_STATE_IN_SEQUENCE, /**< In a sequence. */
+	CYAML_STATE__COUNT,      /**< Count of states, **not a valid
+	                              state itself**. */
+};
+
+/**
+ * Convert a CYAML state into a human readable string.
+ *
+ * \param[in]  state  The state to convert.
+ * \return String representing state.
+ */
+static inline const char * cyaml__state_to_str(enum cyaml_state_e state)
+{
+	static const char * const strings[CYAML_STATE__COUNT] = {
+		[CYAML_STATE_START]       = "start",
+		[CYAML_STATE_IN_STREAM]   = "in stream",
+		[CYAML_STATE_IN_DOC]      = "in doc",
+		[CYAML_STATE_IN_MAPPING]  = "in mapping",
+		[CYAML_STATE_IN_SEQUENCE] = "in sequence",
+	};
+	if ((unsigned)state >= CYAML_STATE__COUNT) {
+		return "<invalid>";
+	}
+	return strings[state];
+}
+
 /**
  * Log to client's logging function, if provided.
  *
