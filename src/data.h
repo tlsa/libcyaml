e593bf2e04105d952dcb807091b7c5abf5f08ff8
Michael Drake <tlsa@netsurf-browser.org>
2019-11-16 11:58:07 +0000

    Data: Squash Wconversion warnings.
    The uint8_t casts are redundant, because the masking by 0xff makes
    the intent clear, but the compiler isn't clever enough to understand
    this.
    
    The entry_size parameters have been changed from uint8_t to uint64_t
    simply to avoid casting at the call sites.

--- src/data.h
+++ src/data.h
@@ -18,27 +18,31 @@
 /**
  * Write a value of up to eight bytes to data_target.
  *
- * \param[in]  value        The value to write.
- * \param[in]  entry_size   The number of bytes of value to write.
- * \param[in]  data_target  The address to write to.
+ * \param[in]  value       The value to write.
+ * \param[in]  entry_size  The number of bytes of value to write.
+ * \param[in]  data_tgt    The address to write to.
  * \return \ref CYAML_OK on success, or appropriate error code otherwise.
  */
 static inline cyaml_err_t cyaml_data_write(
 		uint64_t value,
-		uint8_t entry_size,
-		uint8_t *data_target)
+		uint64_t entry_size,
+		uint8_t *data_tgt)
 {
-	data_target += entry_size - 1;
+	if (entry_size == 0) {
+		return CYAML_ERR_INVALID_DATA_SIZE;
+	}
+
+	data_tgt += entry_size - 1;
 
 	switch (entry_size) {
-	case 8: *data_target-- = (value >> 56) & 0xff; /* Fall through. */
-	case 7: *data_target-- = (value >> 48) & 0xff; /* Fall through. */
-	case 6: *data_target-- = (value >> 40) & 0xff; /* Fall through. */
-	case 5: *data_target-- = (value >> 32) & 0xff; /* Fall through. */
-	case 4: *data_target-- = (value >> 24) & 0xff; /* Fall through. */
-	case 3: *data_target-- = (value >> 16) & 0xff; /* Fall through. */
-	case 2: *data_target-- = (value >>  8) & 0xff; /* Fall through. */
-	case 1: *data_target-- = (value >>  0) & 0xff;
+	case 8: *data_tgt-- = (uint8_t)(value >> 56) & 0xff; /* Fall through. */
+	case 7: *data_tgt-- = (uint8_t)(value >> 48) & 0xff; /* Fall through. */
+	case 6: *data_tgt-- = (uint8_t)(value >> 40) & 0xff; /* Fall through. */
+	case 5: *data_tgt-- = (uint8_t)(value >> 32) & 0xff; /* Fall through. */
+	case 4: *data_tgt-- = (uint8_t)(value >> 24) & 0xff; /* Fall through. */
+	case 3: *data_tgt-- = (uint8_t)(value >> 16) & 0xff; /* Fall through. */
+	case 2: *data_tgt-- = (uint8_t)(value >>  8) & 0xff; /* Fall through. */
+	case 1: *data_tgt-- = (uint8_t)(value >>  0) & 0xff;
 		break;
 	default:
 		return CYAML_ERR_INVALID_DATA_SIZE;
@@ -81,11 +85,16 @@ static inline void cyaml_data_write_pointer(
  *         On failure, returns 0.
  */
 static inline uint64_t cyaml_data_read(
-		uint8_t entry_size,
+		uint64_t entry_size,
 		const uint8_t *data,
 		cyaml_err_t *error_out)
 {
 	uint64_t ret = 0;
+
+	if (entry_size == 0) {
+		*error_out = CYAML_ERR_INVALID_DATA_SIZE;
+		return ret;
+	}
 
 	data += entry_size - 1;
 
