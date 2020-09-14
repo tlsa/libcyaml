bdeb4144de21eaaec17d702ea215413a07a1b9b5
Michael Drake <tlsa@netsurf-browser.org>
2020-09-14 15:54:41 +0100

    Tests: Save: Cast string literals used for reference data.

--- test/units/save.c
+++ test/units/save.c
@@ -1770,11 +1770,11 @@ static bool test_save_mapping_entry_sequence_string_ptr(
 		uint32_t seq_count;
 	} data = {
 		.seq = {
-			"This",
-			"is",
-			"merely",
-			"a",
-			"test", },
+			(char *) "This",
+			(char *) "is",
+			(char *) "merely",
+			(char *) "a",
+			(char *) "test", },
 		.seq_count = CYAML_ARRAY_LEN(data.seq),
 	};
 	static const struct cyaml_schema_value entry_schema = {
